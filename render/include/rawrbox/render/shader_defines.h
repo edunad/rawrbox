97697634a0543a5845df518e2c4df164509e5a68
FailCake <edunad@users.noreply.github.com>
2023-04-08 17:58:07 +0200

    Fix includes

--- render/include/rawrbox/render/shader_defines.h
+++ render/include/rawrbox/render/shader_defines.h
@@ -14,3 +14,15 @@
 	|| BX_PLATFORM_XBOXONE              \
 	)
 // ----------------
+
+#if BGFX_PLATFORM_SUPPORTS_DX9BC
+#	undef  BGFX_EMBEDDED_SHADER_DX9BC
+#	define BGFX_EMBEDDED_SHADER_DX9BC(_renderer, _name) \
+	{ _renderer, BX_CONCATENATE(_name, _dx9 ), BX_COUNTOF(BX_CONCATENATE(_name, _dx9 ) ) },
+#endif // BGFX_PLATFORM_SUPPORTS_DX9BC
+
+#if BGFX_PLATFORM_SUPPORTS_DXBC
+#	undef  BGFX_EMBEDDED_SHADER_DXBC
+#	define BGFX_EMBEDDED_SHADER_DXBC(_renderer, _name) \
+	{ _renderer, BX_CONCATENATE(_name, _dx11), BX_COUNTOF(BX_CONCATENATE(_name, _dx11) ) },
+#endif // BGFX_PLATFORM_SUPPORTS_DXBC
