8a57a1fc3c7be29206f4273d649f143cfed0b117
FailCake <edunad@users.noreply.github.com>
2023-06-03 17:12:54 +0200

    Fix lint

--- utils/include/rawrbox/utils/time_utils.hpp
+++ utils/include/rawrbox/utils/time_utils.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 
 namespace rawrbox {
+	// NOLINTBEGIN(clang-diagnostic-unused-function)
 	static int64_t time() {
 		static std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 		return (std::chrono::high_resolution_clock::now() - startTime).count();
@@ -12,4 +13,5 @@ namespace rawrbox {
 		auto epoch = std::chrono::high_resolution_clock::now().time_since_epoch();
 		return std::chrono::duration_cast<std::chrono::milliseconds>(epoch).count();
 	}
+	// NOLINTEND(clang-diagnostic-unused-function)
 } // namespace rawrbox
