e2e57b3b4d5cec267c59f165feaa2990acdb39fb
FailCake <edunad@users.noreply.github.com>
2023-06-04 16:48:56 +0200

    CMake fixes

--- cmake-format.py
+++ cmake-format.py
@@ -6,6 +6,7 @@ with section("format"):
   separate_fn_name_with_space = False
   dangle_parens = False
   max_subgroups_hwrap = 2
+  command_case = 'unchanged'
 
 # ------------------------------------------------
 # Options affecting comment reflow and formatting.
