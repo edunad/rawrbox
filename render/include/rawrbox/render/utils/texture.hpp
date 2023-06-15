eb985151377387ecb97497e9a3e4213273d4a599
FailCake <edunad@users.noreply.github.com>
2023-06-15 21:13:16 +0200

    Fix build

--- render/include/rawrbox/render/utils/texture.hpp
+++ render/include/rawrbox/render/utils/texture.hpp
@@ -2,12 +2,13 @@
 
 #include <rawrbox/math/vector4.hpp>
 
+#include <cstdint>
+
 namespace rawrbox {
 
 	class TextureUtils {
 	public:
 		// ---
 		static rawrbox::Vector4f atlasToUV(const rawrbox::Vector2i& atlasSize, uint32_t spriteSize, size_t id);
-		static rawrbox::Vector4f atlasToShader(const rawrbox::Vector2i& atlasSize, uint32_t spriteSize, size_t id);
 	};
 } // namespace rawrbox
--- render/src/utils/texture.cpp
+++ render/src/utils/texture.cpp
@@ -18,19 +18,4 @@ namespace rawrbox {
 		// -----------
 		return {uvS.x, uvS.y, uvE.x, uvE.y};
 	};
-
-	rawrbox::Vector4f TextureUtils::atlasToShader(const rawrbox::Vector2i& atlasSize, uint32_t spriteSize, size_t id) {
-		rawrbox::Vector2i totalSprites = atlasSize / spriteSize;
-
-		uint32_t spriteId = std::clamp<uint32_t>(id, 0, (totalSprites.x * totalSprites.y));
-		rawrbox::Vector2f spriteSizeInUV = {static_cast<float>(spriteSize) / atlasSize.x, static_cast<float>(spriteSize) / atlasSize.y};
-
-		auto Y = static_cast<uint32_t>(std::floor(spriteId / totalSprites.x));
-		auto X = spriteId - Y * totalSprites.x;
-
-		rawrbox::Vector2f uvS = spriteSizeInUV * Vector2f(static_cast<float>(X), static_cast<float>(Y));
-
-		return {uvS.x, uvS.y, static_cast<float>(totalSprites.x) / 2, 0}; // Because bgfx shaders only like floats :p
-	}
-
 } // namespace rawrbox
