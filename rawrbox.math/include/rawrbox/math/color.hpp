691a2503d5763e80de5c61f21e9ba1470128ce31
FailCake <edunad@users.noreply.github.com>
2024-02-03 10:52:22 +0100

    Fix color warning

--- rawrbox.math/include/rawrbox/math/color.hpp
+++ rawrbox.math/include/rawrbox/math/color.hpp
@@ -56,7 +56,7 @@ namespace rawrbox {
 
 		[[nodiscard]] NumberType LinearToGamma(const NumberType& x) const {
 			auto val = static_cast<float>(x);
-			return val <= 0.0031308 ? val * 12.92F : 1.055F * std::pow(val, 1.F / 2.4F) - 0.055F;
+			return val <= 0.0031308 ? static_cast<NumberType>(val * 12.92F) : static_cast<NumberType>(1.055F * std::pow(val, 1.F / 2.4F) - 0.055F);
 		}
 
 		[[nodiscard]] Color_t<NumberType> toLinear() const {
