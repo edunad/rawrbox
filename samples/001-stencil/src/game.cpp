426c860c0ffd2b2fcb899217b69e617ad9c3a126
FailCake <edunad@users.noreply.github.com>
2024-03-01 14:26:30 +0100

    Revert sample 001-stencil

--- samples/001-stencil/src/game.cpp
+++ samples/001-stencil/src/game.cpp
@@ -21,7 +21,7 @@ namespace stencil {
 #else
 		auto window = rawrbox::Window::createWindow();
 #endif
-		window->setMonitor(1);
+		window->setMonitor(-1);
 		window->setTitle("STENCIL TEST");
 #ifdef _DEBUG
 		window->init(1024, 768, rawrbox::WindowFlags::Window::WINDOWED);
