7868f7edefd60d3a5f5cc36187689e64c96617ee
FailCake <edunad@users.noreply.github.com>
2024-08-10 11:07:55 +0200

    Update packages

--- package-lock.cmake
+++ package-lock.cmake
@@ -17,7 +17,7 @@ CPMDeclarePackage(magic_enum
 )
 # glaze
 CPMDeclarePackage(glaze
-  VERSION 3.2.1
+  VERSION 3.2.2
   GITHUB_REPOSITORY stephenberry/glaze
   SYSTEM YES
   EXCLUDE_FROM_ALL YES
@@ -77,7 +77,7 @@ CPMDeclarePackage(cpptrace
 # luau
 CPMDeclarePackage(luau
   NAME luau
-  GIT_TAG 0.637
+  GIT_TAG 0.638
   GITHUB_REPOSITORY luau-lang/luau
   OPTIONS
     "LUAU_BUILD_CLI OFF"
